1c6b466fa5a92ce44d6cf2c12913095524ec4978
CyndiCodes <huertascyn@gmail.com>
2023-08-05 11:40:23 -0400

    started click features and html layout. currently not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 let apiKey = "ae43fce93221a7479e25011f753d1c95";
 let city = "";
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
 
-let latLonQuery =  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+
+
 // this one used in video
 
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -27,23 +27,68 @@ let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{stat
 // Next, we concatenate the other required parameter, appid=, where we'll add the API key specific to the application.
 
 // Finally, we concatenate the APIKey variable that contains the key we obtained at the beginning of this guide.
+let cityFormEl = document.querySelector("#city-form");
+let cityBtnEl = document.querySelector("#city-buttons");
+let cityName = document.querySelector("#cityName");
+let weatherContainerEl = document.querySelector("weather-container");
+let cityInputEl = document.querySelector("#cityName");
+let citySearched = document.querySelector("#city-searched");
 
-fetch(latLonQuery)
+
+let formSubmitHandler = function (event) {
+   event.preventDefault();
+   
+   let cityName = cityInputEl.value.trim();
+   
+   if (cityName) {
+       getWeather(cityName);
+       
+       weatherContainerEl.textContent = '';
+       cityInputEl.value = '';
+    } else {
+        alert('Please enter a city or zip code');
+    }
+};
+
+// let buttonClickHandler = function (event) {
+//     let city = event.target.getAttribute('data-city');
+    
+//     if (city) {
+//         getSearchedWeather(city);
+        
+//         weatherContainerEl.textContent = '';
+//     }
+//  };
+ 
+ let getWeather = function (city){
+     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+     let latLonQuery =  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+     
+    fetch(latLonQuery)
     .then(response => response.json())
     .then(citiesFound => {
         let firstCity = citiesFound[0];
+        if(response.ok){
+            displayWeather(citiesFound, firstCity)
+        }
         console.log(citiesFound)
         console.log(firstCity)
-        console.log(firstCity.lat)
-        console.log(firstCity.lon)
+            console.log(firstCity.lat)
+            console.log(firstCity.lon)
+    
+            return fetch(queryURL) 
+        })
+    
+    
+    .then(response => response.json())
+    .then(data => {
+    
+        console.log(data)
+    
+      })
+}
 
-        return fetch(geoQuery) 
-    })
+let displayWeather = function ()
 
-
-.then(response => response.json())
-.then(data => {
-
-    console.log(data)
-
-  })
+cityFormEl.addEventListener("searchBtn", formSubmitHandler);
+cityBtnEl.addEventListener("click", buttonClickHandler);
