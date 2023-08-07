835bb8070198ef28d005778f8d3292aad3727f77
CyndiCodes <huertascyn@gmail.com>
2023-08-07 18:16:46 -0400

    get weather now working, populating on current seacrh in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,73 @@
 let apiKey = "ae43fce93221a7479e25011f753d1c95";
 let city = "";
-
-
-
 let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 
-
-// this one used in video
-
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-// q: The query parameter, where we'll add the city variable.
-
-// appid: The application id or key, where we'll add the API key variable.
-
-// (?) marks the boundary between the base URL of the API call and the query terms of the API call
-
-// http://api.openweathermap.org/data/2.5/weather is the base URL for calling the Current Weather Data API.
-// q= is the query parameter, where we can add any user input to specify the data that we want to request in the API call. The value assigned to this parameter is called the query string.
-
-
-// Following the query parameter, we concatenate the user input, which is stored in the variable city. This is the query string assigned to the query parameter.
-
-// The ampersand character (&) indicates that we're adding another parameter after the query parameter.
-
-// Next, we concatenate the other required parameter, appid=, where we'll add the API key specific to the application.
-
-// Finally, we concatenate the APIKey variable that contains the key we obtained at the beginning of this guide.
 let cityFormEl = document.querySelector("#city-form");
 let cityBtnEl = document.querySelector("#city-buttons");
 let cityName = document.querySelector("#cityName");
 let weatherContainerEl = document.querySelector("weather-container");
 let cityInputEl = document.querySelector("#cityName");
 let citySearched = document.querySelector("#city-searched");
+let currentCityEl = document.getElementById("city");
+let temp = document.getElementById("temperature");
+let wind = document.getElementById("wind-speed");
+let humidity = document.getElementById("humidity");
+
+let getWeather = function (city){
+    //  let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.name + "&appid=" + apiKey;
+    //  let latLonQuery =  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+     
+     fetch("http://api.openweathermap.org/geo/1.0/direct?q=trenton&limit=5&appid=ae43fce93221a7479e25011f753d1c95")
+     .then(response => response.json())
+     .then(citiesFound =>{
+             let firstCity = citiesFound[0];
+             console.log(citiesFound)
+             console.log(firstCity)
+             console.log(firstCity.lat)
+             console.log(firstCity.lon)
+     
+             return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=ae43fce93221a7479e25011f753d1c95`)
+
+         })
+     
+         .then(response => response.json())
+         .then(weatherData =>{
+             console.log(weatherData)
+             let tempFound = weatherData.list[0].main.temp;
+             temp.textContent = "Temperature: "+ tempFound;
+             console.log(tempFound)
+             let windFound = weatherData.list[0].wind.speed
+             wind.textContent = "Wind Speed: " + windFound;
+             console.log(windFound)
+             let humFound = weatherData.list[0].main.humidity
+             humidity.textContent =  "Humidity: " +humFound;
+             console.log(humFound)
+         })
+        }
+     
+getWeather()
 
 
-let formSubmitHandler = function (event) {
-   event.preventDefault();
+
+
+
+
+
+
+// let formSubmitHandler = function (event) {
+//    event.preventDefault();
    
-   let cityName = cityInputEl.value.trim();
+//    let cityName = cityInputEl.value.trim();
    
-   if (cityName) {
-       getWeather(cityName);
+//    if (cityName) {
+//        getWeather(cityName);
        
-       weatherContainerEl.textContent = '';
-       cityInputEl.value = '';
-    } else {
-        alert('Please enter a city or zip code');
-    }
-};
+//        weatherContainerEl.textContent = '';
+//        cityInputEl.value = '';
+//     } else {
+//         alert('Please enter a city or zip code');
+//     }
+// };
 
 // let buttonClickHandler = function (event) {
 //     let city = event.target.getAttribute('data-city');
@@ -60,35 +79,22 @@ let formSubmitHandler = function (event) {
 //     }
 //  };
  
- let getWeather = function (city){
-     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-     let latLonQuery =  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-     
-    fetch(latLonQuery)
-    .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        if(response.ok){
-            displayWeather(citiesFound, firstCity)
-        }
-        console.log(citiesFound)
-        console.log(firstCity)
-            console.log(firstCity.lat)
-            console.log(firstCity.lon)
+// let inputFormBtn = document.getElementsById("cityName")
     
-            return fetch(queryURL) 
-        })
+//     .then(response => response.json())
+//     .then(data => {
+//         getWeather();
+//         weatherContainerEl.textContent = "";
+//         console.log(data)
     
     
-    .then(response => response.json())
-    .then(data => {
-    
-        console.log(data)
-    
-      })
-}
+//       })
+// }
 
-let displayWeather = function ()
+// let displayWeather = function (){
 
-cityFormEl.addEventListener("searchBtn", formSubmitHandler);
-cityBtnEl.addEventListener("click", buttonClickHandler);
+// }
+
+// inputFormBtn.addEventListener("click", function (event){
+    
+// })
