6b0383ca8b7d4bc01d57c7d82cc6074da711d209
CyndiCodes <huertascyn@gmail.com>
2023-09-16 19:10:40 -0400

    btns do not get created mulitple times now, and you cannot search undefined fields to render empty btns, bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,27 @@ let apiKey = "ae43fce93221a7479e25011f753d1c95";
 let cityName = document.querySelector("#cityName");
 let storedCities = document.getElementById("storedCities")
 let cityInputEl = document.getElementById("cityInput");
-
 let form = document.getElementById("city-form");
 let currentDate =dayjs().format('MMM DD, YYYY');
 let currentDateEl = document.getElementById("currentDay")
 currentDateEl.textContent = currentDate;
 
-form.addEventListener('submit', handleFormSubmit)
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const city = cityInputEl.value.trim();
+
+    if (city) {
+        // Only run the functions if city is defined and not empty
+        console.log(event.textContent);
+        handleFormSubmit(event);
+        createBtn(city);
+    }
+
+    // Reset the input field
+    cityInputEl.value = "";
+});
+
 
 function handleFormSubmit(event){
     
@@ -17,25 +31,34 @@ function handleFormSubmit(event){
     document.getElementById('currentWeather').innerHTML = '';
     document.getElementById('forecastWeather').innerHTML = '';
 
-    let city = cityInputEl.value.trim()
+    const city = cityInputEl.value.trim()
 
     runDailyWeather(city)
     runForecast(city)
-    createBtn(city)
-    cityInputEl.value = ""
+    
 }
 
-function createBtn(){
-    let newCityBtn = document.createElement("button");
-    storedCities.append(newCityBtn)
-    newCityBtn.textContent = cityInputEl.value.trim();
-    newCityBtn.addEventListener("click", function (event){
-        event.target.textContent
-        console.log(event.target.textContent)
-        cityInputEl.value = event.target.textContent
-        handleFormSubmit(event)
-    })
+
+function createBtn() {
+    const cityName = cityInputEl.value.trim();
+
+    // Check if a button already exists
+    const existingButton = Array.from(storedCities.children).find(button => button.textContent === cityName);
+
+    if (!existingButton) {
+        let newCityBtn = document.createElement("button");
+        newCityBtn.textContent = cityName;
+        storedCities.append(newCityBtn);
+
+        newCityBtn.addEventListener("click", function (event) {
+            event.target.textContent;
+            console.log(event.target.textContent);
+            cityInputEl.value = event.target.textContent;
+            handleFormSubmit(event);
+        });
+    }
 }
+
 
 function runDailyWeather(city){
     let url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`+apiKey+"&units=imperial";
@@ -63,9 +86,9 @@ function runDailyWeather(city){
         cardHumid.setAttribute('class', 'card-text')
 
         cardTitle.textContent = weatherData.name
-        cardTemp.textContent = "Temperature: "+weatherData.main.temp
-        cardWind.textContent = "Wind: "+weatherData.wind.speed
-        cardHumid.textContent = "Humidity: "+weatherData.main.humidity
+        cardTemp.textContent = "Temperature: "+weatherData.main.temp + " Fahrenheit"
+        cardWind.textContent = "Wind: "+weatherData.wind.speed  + " MPH"
+        cardHumid.textContent = "Humidity: "+weatherData.main.humidity  + " %"
 
         cardBody.append(cardTitle, cardTemp, cardWind, cardHumid, cardIcon)
         card.append(cardBody)
@@ -81,41 +104,41 @@ function runForecast(city){
     fetch(forecastUrl)
     .then(res=> res.json())
     .then(weatherData =>{
-                    console.log(weatherData)
-                    var forecastArr = [weatherData.list[7], weatherData.list[15], weatherData.list[23], weatherData.list[31], weatherData.list[39]]
+        console.log(weatherData)
+        var forecastArr = [weatherData.list[7], weatherData.list[15], weatherData.list[23], weatherData.list[31], weatherData.list[39]]
 
-                    for (let index = 0; index < forecastArr.length; index++) {
-                        const card = document.createElement('div')
-                       const cardBody = document.createElement('div')
-                       const cardTitle = document.createElement('h5')
-                       const cardIcon = document.createElement('img')
-                       const cardTemp = document.createElement('p')
-                       const cardWind = document.createElement('p')
-                       const cardHumid = document.createElement('p')
-                       
-                       card.setAttribute('style', 'width:18rem;')
-                       card.setAttribute('class', 'card')
-                        cardBody.setAttribute('class', 'card-body')
-                        cardIcon.setAttribute('src', 'https://openweathermap.org/img/wn/'+forecastArr[index].weather[0].icon+'@2x.png')
-                        cardTitle.setAttribute('class', 'card-header  bg-info-subtle')
-                        cardTemp.setAttribute('class', 'card-text')
-                        cardWind.setAttribute('class', 'card-text')
-                        cardHumid.setAttribute('class', 'card-text')
-                        
-                        cardTitle.textContent = forecastArr[index].dt_txt
-                        cardTemp.textContent = "Temperature: "+forecastArr[index].main.temp
-                        cardWind.textContent = "Wind: "+forecastArr[index].wind.speed
-                        cardHumid.textContent = "Humidity: "+forecastArr[index].main.humidity
-                        // cardIcon.textContent = forecastArr[index].main.humidity
+        for (let index = 0; index < forecastArr.length; index++) {
+            const card = document.createElement('div')
+            const cardBody = document.createElement('div')
+            const cardTitle = document.createElement('h5')
+            const cardIcon = document.createElement('img')
+            const cardTemp = document.createElement('p')
+            const cardWind = document.createElement('p')
+            const cardHumid = document.createElement('p')
+            
+            card.setAttribute('style', 'width:18rem;')
+            card.setAttribute('class', 'card')
+            cardBody.setAttribute('class', 'card-body')
+            cardIcon.setAttribute('src', 'https://openweathermap.org/img/wn/'+forecastArr[index].weather[0].icon+'@2x.png')
+            cardTitle.setAttribute('class', 'card-header  bg-info-subtle')
+            cardTemp.setAttribute('class', 'card-text')
+            cardWind.setAttribute('class', 'card-text')
+            cardHumid.setAttribute('class', 'card-text')
+            
+            cardTitle.textContent = forecastArr[index].dt_txt
+            cardTemp.textContent = "Temperature: "+forecastArr[index].main.temp + " Fahrenheit"
+            cardWind.textContent = "Wind: "+forecastArr[index].wind.speed+ " MPH"
+            cardHumid.textContent = "Humidity: "+forecastArr[index].main.humidity  + " %"
+            // cardIcon.textContent = forecastArr[index].main.humidity
 
-                        cardBody.append(cardTitle, cardTemp, cardWind, cardHumid, cardIcon)
-                        card.append(cardBody)
-                        document.getElementById('forecastWeather').append(card)
-                    }
-                    
+            cardBody.append(cardTitle, cardTemp, cardWind, cardHumid, cardIcon)
+            card.append(cardBody)
+            document.getElementById('forecastWeather').append(card)
+        }
         
-                    localStorage.setItem("Weather", JSON.stringify(weatherData));
-        
+
+        localStorage.setItem("Weather", JSON.stringify(weatherData));
+
         })
 }
 
