c712edff92dc0e0a6f5a065f55e992a31fa29dd7
CyndiCodes <huertascyn@gmail.com>
2023-09-16 14:29:03 -0400

    add btn once again working after search, cleaned html and removed old hard code no longer needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,126 @@
 let apiKey = "ae43fce93221a7479e25011f753d1c95";
 let cityName = document.querySelector("#cityName");
-let temp = document.getElementById("temperature");
-let wind = document.getElementById("wind-speed");
-let humidity = document.getElementById("humidity");
-let icon = document.getElementById("icon");
-let city = document.getElementById("city");
-
+let storedCities = document.getElementById("storedCities")
 let cityInputEl = document.getElementById("cityInput");
 
 let form = document.getElementById("city-form");
 let currentDate =dayjs().format('MMM DD, YYYY');
 let currentDateEl = document.getElementById("currentDay")
-// currentDateEl.textContent = "Date: " +currentDate;
+currentDateEl.textContent = currentDate;
 
 function handleFormSubmit(event){
-
+    
     event.preventDefault();
     
     let city = cityInputEl.value.trim()
-
     runDailyWeather(city)
     runForecast(city)
 }
 
+function createBtn(){
+    let newCityBtn = document.createElement("button");
+    storedCities.append(newCityBtn)
+    newCityBtn.textContent = cityInputEl.value.trim();
+    newCityBtn.addEventListener("click", function (event){
+        event.target
+
+})    
+}
+
+function runDailyWeather(city){
+    let url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`+apiKey+"&units=imperial";
+
+    fetch(url)
+    .then(res=> res.json())
+    .then(weatherData =>{
+                    console.log(weatherData)
+                    console.log(weatherData.name)
+                    console.log(weatherData.main.temp)
+                    console.log(weatherData.wind.speed)
+                    console.log(weatherData.weather[0].icon)
+                    console.log(weatherData.main.humidity)
+        
+                    const container = document.createElement('div')
+                    const card = document.createElement('div')
+                    const cardTitle = document.createElement('h2')
+                    const cardBody = document.createElement('div')
+                    const cardCity = document.createElement('p')
+                    const cardTemp = document.createElement('p')
+                    const cardWind = document.createElement('p')
+                    const cardHumid = document.createElement('p')
+
+                    container.setAttribute('class', "card")
+                    // card.setAttribute('style', 'width:18rem;')
+                    card.setAttribute('class', 'col-12 col-md-8')
+                     cardBody.setAttribute('class', 'card-body')
+                     cardTitle.setAttribute('class', 'card-header  bg-info-subtle')
+                     cardTemp.setAttribute('class', 'card-text')
+                     cardWind.setAttribute('class', 'card-text')
+                     cardHumid.setAttribute('class', 'card-text')
+
+                     cardTitle.textContent = weatherData.name
+                     cardTemp.textContent = weatherData.main.temp
+                     cardWind.textContent = weatherData.wind.speed
+                     cardHumid.textContent = weatherData.main.humidity
+
+                     cardBody.append(cardTitle, cardTemp, cardWind, cardHumid)
+                        card.append(cardBody)
+                        document.getElementById('currentWeather').append(card)
+            // localStorage.setItem("Weather", JSON.stringify(weatherData));
+            // createBtn(city)
+        
+      
+        
+})
+}
+   
+
+function runDailyWeather(city){
+    let url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`+apiKey+"&units=imperial";
+
+    fetch(url)
+    .then(res=> res.json())
+    .then(weatherData =>{
+                    console.log(weatherData)
+                    console.log(weatherData.name)
+                    console.log(weatherData.main.temp)
+                    console.log(weatherData.wind.speed)
+                    console.log(weatherData.weather[0].icon)
+                    console.log(weatherData.main.humidity)
+        
+                    // const container = document.createElement('div')
+                    const card = document.createElement('div')
+                    const cardTitle = document.createElement('h2')
+                    const cardBody = document.createElement('div')
+                    const cardCity = document.createElement('p')
+                    const cardTemp = document.createElement('p')
+                    const cardWind = document.createElement('p')
+                    const cardHumid = document.createElement('p')
+
+                    // container.setAttribute('class', "card")
+                    // card.setAttribute('style', 'width:18rem;')
+                    card.setAttribute('class', 'col-12 col-md-8')
+                     cardBody.setAttribute('class', 'card-body')
+                     cardTitle.setAttribute('class', 'card-header  bg-info-subtle')
+                     cardTemp.setAttribute('class', 'card-text')
+                     cardWind.setAttribute('class', 'card-text')
+                     cardHumid.setAttribute('class', 'card-text')
+
+                     cardTitle.textContent = weatherData.name
+                     cardTemp.textContent = weatherData.main.temp
+                     cardWind.textContent = weatherData.wind.speed
+                     cardHumid.textContent = weatherData.main.humidity
+
+                     cardBody.append(cardTitle, cardTemp, cardWind, cardHumid)
+                        card.append(cardBody)
+                        document.getElementById('currentWeather').append(card)
+            localStorage.setItem("Weather", JSON.stringify(weatherData));
+            createBtn(city)
+        
+      
+        
+})
+}
 
 function runForecast(city){
     let forecastUrl =  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=`+apiKey+"&units=imperial";
@@ -69,153 +167,10 @@ function runForecast(city){
 })
 }
 
-function runDailyWeather(city){
-    let url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`+apiKey+"&units=imperial";
-
-    fetch(url)
-    .then(res=> res.json())
-    .then(weatherData =>{
-                    console.log(weatherData)
-                    console.log(weatherData.name)
-                    console.log(weatherData.main.temp)
-                    console.log(weatherData.wind.speed)
-                    console.log(weatherData.weather[0].icon)
-                    console.log(weatherData.main.humidity)
-        
-       
-                    const card = document.createElement('div')
-                    const cardTitle = document.createElement('h2')
-                    const cardBody = document.createElement('div')
-                    const cardCity = document.createElement('p')
-                    const cardTemp = document.createElement('p')
-                    const cardWind = document.createElement('p')
-                    const cardHumid = document.createElement('p')
-
-                    card.setAttribute('style', 'width:18rem;')
-                    card.setAttribute('class', 'col-12 col-md-8')
-                     cardBody.setAttribute('class', 'card-body')
-                     cardTitle.setAttribute('class', 'card-header  bg-info-subtle')
-                     cardTemp.setAttribute('class', 'card-text')
-                     cardWind.setAttribute('class', 'card-text')
-                     cardHumid.setAttribute('class', 'card-text')
-
-                     cardTitle.textContent = weatherData.name
-                     cardTemp.textContent = weatherData.main.temp
-                     cardWind.textContent = weatherData.wind.speed
-                     cardHumid.textContent = weatherData.main.humidity
-
-                     cardBody.append(cardTitle, cardTemp, cardWind, cardHumid)
-                        card.append(cardBody)
-                        document.getElementById('currentWeather').append(card)
-            // localStorage.setItem("Weather", JSON.stringify(weatherData));
-            // createBtn(city)
-        
-      
-        
-})
-}
-
-
 form.addEventListener('submit', handleFormSubmit)
-// form.addEventListener("submit", function (event){
-//     event.preventDefault();
-//     let city = cityInputEl.value.trim();
-    
-//     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
-//     .then(response => response.json())
-//     .then(citiesFound =>{
-//         let firstCity = citiesFound[0];
-//         console.log(citiesFound)
-//             console.log(firstCity)
-//             console.log(firstCity.lat)
-//             console.log(firstCity.lon)
-//             console.log(firstCity.name)
-            
-            
-//             let latLonQuery =  `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&cnt=6&appid=`+apiKey+"&units=imperial";
-            
-//             return fetch(latLonQuery)
-//             //  units=imperial for weather in Fahrenheit
-//             // according to documentation - cnt is supposed to be for day but is showing every 3 hours in console
-//         })
-        
-//         .then(response => response.json())
-//         .then(weatherData =>{
-//             console.log(weatherData)
+  
 
-//            let cityFound = weatherData.city.name
-//            city.textContent= "City: " +cityFound;
-//             console.log (weatherData.city.name )
-            
-//             let tempFound = weatherData.list[0].main.temp;
-//             temp.textContent = "Temperature: "+ tempFound + " Fahrenheit";
-            
-//             let windFound = weatherData.list[0].wind.speed
-//             wind.textContent = "Wind Speed: " + windFound + " MPH";
-            
-//             let humFound = weatherData.list[0].main.humidity
-//             humidity.textContent =  "Humidity: " +humFound + " %";
-            
-//             let iconEl = weatherData.list[0].weather.icon;
-//             icon.value = iconEl
-            
-//     if (city === '') {    
-//         return;
-//     }    
 
-//     localStorage.setItem("Weather", JSON.stringify(weatherData));
-//     createBtn(city)
 
-// })    
-
-// let storedCities = document.getElementById("storedCities")
-// function createBtn(){
-//     let newCityBtn = document.createElement("button");
-//     storedCities.append(newCityBtn)
-//     newCityBtn.textContent = cityInputEl.value.trim();
-//     newCityBtn.addEventListener("click", function (event){
-//         event.target
-//         fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
-//     .then(response => response.json())
-//     .then(citiesFound =>{
-//         let firstCity = citiesFound[0];
-//         console.log(citiesFound)
-//             console.log(firstCity)
-//             console.log(firstCity.lat)
-//             console.log(firstCity.lon)
-            
-//             let latLonQuery =  `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&cnt=6&appid=`+apiKey+"&units=imperial";
-    
-//             return fetch(latLonQuery)
-//             //  units=imperial for weather in Fahrenheit
-//             // according to documentation - cnt is supposed to be for day but is showing every 3 hours in console
-//         })
-        
-//         .then(response => response.json())
-//         .then(weatherData =>{
-//             console.log(weatherData)
-
-//             let tempFound = weatherData.list[0].main.temp;
-//             temp.textContent = "Temperature: "+ tempFound + " Fahrenheit";
-            
-//             let windFound = weatherData.list[0].wind.speed
-//             wind.textContent = "Wind Speed: " + windFound + " MPH";
-            
-//             let humFound = weatherData.list[0].main.humidity
-//             humidity.textContent =  "Humidity: " +humFound + " %";
-            
-//             let iconEl = weatherData.list[0].weather.icon;
-//             icon.value = iconEl
-            
-//     if (city === '') {    
-//         return;
-//     }    
-
-//     localStorage.setItem("Weather", JSON.stringify(weatherData));
-
-// })    
-//     })
-// }
-// })
 
 
